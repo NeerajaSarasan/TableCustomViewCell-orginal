947585e043154b88f7bf5b1dce09a4eb450c328e
Neeraja Sarasan <neeraja.20002023@gmail.com>
2023-09-08 17:22:06 +0530

    Initial Commit

---
+++ TableCustomViewCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableCustomViewCell
+//
+//  Created by Neeraja Sarasan on 08/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
